c613145819acb660343cf3651b3fe53063c88ccb
pedrocncosta <pedrocncosta@gmail.com>
2022-05-31 09:57:09 +0100

    Done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const User = require("../models/User.model");
 
-router.get('/user/:userId', (req, res, next) => {
-  res.status(200).json('Should get the specific user info');
+router.get("/user/:userId", (req, res, next) => {
+  res.status(200).json("Should get the specific user info");
+});
+
+router.put("/user/:userid", (req, res, next) => {
+  const { username, email, password, image, favourites } = req.body;
+  User.create({ username, email, password, image, favourites })
+    .then((response) => res.json(response))
+    .catch((err) => res.status(400).json({ message: "No user updated" }));
 });
 
 module.exports = router;
