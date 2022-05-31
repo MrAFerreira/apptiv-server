27c002de09ac57bbf53a088d301f52d922886062
FranciscoGBento <f_m_bento@hotmail.com>
2022-05-31 09:43:44 +0100

    getUserRoute

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const User = require("../models/User.model");
 
-router.get('/user/:userId', (req, res, next) => {
-  res.status(200).json('Should get the specific user info');
-});
+router
+  .get("/user/:userId", (req, res, next) => {
+    const { userId } = req.body;
+    User.findById({ userId }).then((userFromDB) =>
+      res.status(200).json(userFromDB)
+    );
+  })
+  .catch((err) => res.status(400).json({ message: "User not found!!" }));
 
 module.exports = router;
