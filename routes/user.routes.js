7ad6f159d174895d2b96d41e9346ad8022ac450e
keven andrade <keven111299@gmail.com>
2022-05-31 10:08:32 +0100

    delete user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 const fileUploader = require("../config/cloudinary.config");
-
+const User = require("../models/User.model");
 router.get("/user/:userId", (req, res, next) => {
   res.status(200).json("Should get the specific user info");
+});
+
+router.delete("/user/:userId/Delete", (req, res, next) => {
+  const { userId } = req.params;
+  User.findByIdAndRemove(userId)
+    .then((response) => res.json(response))
+    .catch((err) =>
+      res.status(400).json({ message: "Invalid username supplied" })
+    );
 });
 
 module.exports = router;
