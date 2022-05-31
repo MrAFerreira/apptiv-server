fa7c80417386aa45718a2dbd415d4cbfaecece5b
keven andrade <keven111299@gmail.com>
2022-05-31 11:38:35 +0100

    cloudinary middleware on the routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -11,23 +11,21 @@ router.get("/events", (req, res, next) => {
       res.status(200).json(allEvents);
     })
     .catch((err) => res.status(400).json({ message: "No events were found" }));
+});
 
-
-
-router.delete('/events/:eventId', (req, res, next) => {
+router.delete("/events/:eventId", (req, res, next) => {
   const { eventId } = req.params;
 
   Event.findByIdAndRemove(eventId)
     .then((response) => res.json(response))
-    .catch((err) => res.status(400).json({ message: 'Error Message' }));
+    .catch((err) => res.status(400).json({ message: "Error Message" }));
 });
 
-router.get('/events/:eventId', (req, res, next) => {
+router.get("/events/:eventId", (req, res, next) => {
   const { eventId } = req.params;
   Event.findById(eventId)
     .then((response) => res.json(response))
-    .catch((err) => res.status(405).json({ message: 'Invalid Input' }));
+    .catch((err) => res.status(405).json({ message: "Invalid Input" }));
 });
-
 
 module.exports = router;
