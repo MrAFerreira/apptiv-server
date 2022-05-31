a8425cfb0c2d6520f3b530cb780c5d603d26ad96
keven andrade <keven111299@gmail.com>
2022-05-31 12:03:05 +0100

    user able to edit image

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const fileUploader = require('../config/cloudinary.config');
-const User = require('../models/User.model');
+const router = require("express").Router();
+const fileUploader = require("../config/cloudinary.config");
+const User = require("../models/User.model");
 
-router.get('/user/:userId', (req, res, next) => {
-  res.status(200).json('Should get the specific user info');
+router.get("/user/:userId", (req, res, next) => {
+  res.status(200).json("Should get the specific user info");
 });
 
 const User = require("../models/User.model");
@@ -20,26 +20,33 @@ router
   .catch((err) => res.status(400).json({ message: "Friends not found!" }));
 
 router
-  .get('/user/:userId', (req, res, next) => {
+  .get("/user/:userId", (req, res, next) => {
     const { userId } = req.body;
-    User.findById({ userId }).then((userFromDB) => res.status(200).json(userFromDB));
+    User.findById({ userId }).then((userFromDB) =>
+      res.status(200).json(userFromDB)
+    );
   })
-  .catch((err) => res.status(400).json({ message: 'User not found!!' }));
+  .catch((err) => res.status(400).json({ message: "User not found!!" }));
 
-router.put('/user/:userId', (req, res, next) => {
-  const { userId } = req.params;
-  const { username, email, password, image, favourites } = req.body;
-  User.create({ username, email, password, image, favourites })
-    .then((response) => res.json(response))
-    .catch((err) => res.status(400).json({ message: 'No user updated' }));
-});
+router.put(
+  "/user/:userId",
+  fileUploader.single("userImage"),
+  (req, res, next) => {
+    const { userId } = req.params;
+    const { username, email, password, image, favourites } = req.body;
+    User.create({ username, email, password, image, favourites })
+      .then((response) => res.json(response))
+      .catch((err) => res.status(400).json({ message: "No user updated" }));
+  }
+);
 
-router.delete('/user/:userId/delete', (req, res, next) => {
+router.delete("/user/:userId/delete", (req, res, next) => {
   const { userId } = req.params;
   User.findByIdAndRemove(userId)
     .then((response) => res.json(response))
-    .catch((err) => res.status(400).json({ message: 'Invalid username supplied' }));
+    .catch((err) =>
+      res.status(400).json({ message: "Invalid username supplied" })
+    );
 });
-
 
 module.exports = router;
