2ba367ebc7d4a245b5d0feb02b01394ed39bb48c
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-05-31 10:19:41 +0100

    small typo fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,15 @@
-const router = require("express").Router();
-const fileUploader = require("../config/cloudinary.config");
-const User = require("../models/User.model");
-router.get("/user/:userId", (req, res, next) => {
-  res.status(200).json("Should get the specific user info");
+const router = require('express').Router();
+const fileUploader = require('../config/cloudinary.config');
+const User = require('../models/User.model');
+router.get('/user/:userId', (req, res, next) => {
+  res.status(200).json('Should get the specific user info');
 });
 
-router.delete("/user/:userId/Delete", (req, res, next) => {
+router.delete('/user/:userId/delete', (req, res, next) => {
   const { userId } = req.params;
   User.findByIdAndRemove(userId)
     .then((response) => res.json(response))
-    .catch((err) =>
-      res.status(400).json({ message: "Invalid username supplied" })
-    );
+    .catch((err) => res.status(400).json({ message: 'Invalid username supplied' }));
 });
 
 module.exports = router;
