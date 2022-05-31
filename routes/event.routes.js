12315bb3e35d55d686d45027cca38b0053a21783
keven andrade <keven111299@gmail.com>
2022-05-31 11:16:55 +0100

    bug fix get event by id

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,7 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/events', (req, res, next) => {
-  res.status(200).json('Should send all the events');
+router.get("/events", (req, res, next) => {
+  res.status(200).json("Should send all the events");
 });
 
+router.get("/events/eventId", (req, res, next) => {
+  const { eventId } = req.params;
+  Event.findById(eventId)
+    .populate("attendees")
+    .then((response) => res.status(200).json(response))
+    .catch((err) => res.status(405).json({ message: "Invalid input" }));
+});
 module.exports = router;
