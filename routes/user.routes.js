8354d6b4e065e843de523e4473e748f94ce457bc
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-05-31 17:36:15 +0100

    fixes user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,42 +15,49 @@ router.get('/user/:username/friends', (req, res, next) => {
 router.get('/user/:username', (req, res, next) => {
   const { username } = req.params;
   User.find({ username })
+    .populate('attending')
     .then((userFromDB) => res.status(200).json(userFromDB))
     .catch((err) => res.status(400).json({ message: 'User not found!!' }));
 });
 
 router.put('/user/:userId', fileUploader.single('userImage'), (req, res, next) => {
   const { userId } = req.params;
-  const { username, email, password, favourites } = req.body;
+  const { username, email, favourites, attending } = req.body;
   if (req.file) {
     User.findByIdAndUpdate(
       userId,
       {
         username,
         email,
-        password,
         favourites,
         image: req.file.path,
+        $push: { favourites: attending },
       },
       { new: true }
     )
 
       .then((response) => res.json(response))
-      .catch((err) => res.status(400).json({ message: 'No user updated' }));
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json({ message: 'No user updated' });
+      });
   } else {
     User.findByIdAndUpdate(
       userId,
       {
         username,
         email,
-        password,
         favourites,
+        $push: { attending: attending },
       },
       { new: true }
     )
 
       .then((response) => res.json(response))
-      .catch((err) => res.status(400).json({ message: 'No user updated' }));
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json({ message: 'No user updated' });
+      });
   }
 });
 
