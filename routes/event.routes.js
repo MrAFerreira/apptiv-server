ff1c2f995ca5d0f4ef2f16b11c32fe2fdcb5a45d
joaocbantunes <joaocbantunes@gmail.com>
2022-05-31 10:08:42 +0100

    delete event route

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,7 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/events', (req, res, next) => {
-  res.status(200).json('Should send all the events');
+const Event = require("../models/Event.model.js");
+
+router.get("/events", (req, res, next) => {
+  res.status(200).json("Should send all the events");
+});
+
+roouter.delete("/events/:eventId", (req, res, next) => {
+  const { eventId } = req.params;
+
+  Event.findByIdAndRemove(eventId)
+    .then((response) => res.json(response))
+    .catch((err) => res.status(400).json({ message: "Error Message" }));
 });
 
 module.exports = router;
