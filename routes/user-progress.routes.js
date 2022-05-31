46e3ddeffaa18e14e89297abdcd33a1dea712162
joaoGon <jpgon@live.com.pt>
2022-05-31 14:40:54 +0100

    done

--- routes/user-progress.routes.js
+++ routes/user-progress.routes.js
@@ -1,34 +1,35 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
-const Event = require("../models/Event.model.js");
 
 function getWeek(date) {
   let oneJan = new Date(date.getFullYear(), 0, 1);
   let numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000));
   let result = Math.ceil((date.getDay() + 1 + numberOfDays) / 7);
-  console.log(
-    `The week number of the current date (${new Date()}) is ${result}.`
-  );
-}
-let currentWeek = getWeek(new Date());
-
-function allEvents() {
-  User.attending.filter((e) => getWeek(e.startDate) === currentWeek);
-  /*  Math.round((100 * data.attending) / data.goals); */
-}
-
-function eventThisWeek() {
-  allEvents().filter((e) => e.startDate < new Date());
 }
 
 router.get("/user/:userId/progress", (req, res, next) => {
   const { userId } = req.params;
 
-  User.findById({ userId })
-    .populate("goals")
+  let currentWeek = getWeek(new Date());
+
+  function eventsThisWeek() {
+    User.findById(userId)
+      .populate("attending")
+      .then((response) => {
+        return response.attending.filter(
+          (e) => getWeek(e.startDate) === currentWeek
+        );
+      });
+  }
+
+  function passedEvents() {
+    eventsThisWeek().filter((e) => e.startDate < new Date());
+  }
+
+  User.findById(userId)
     .populate("attending")
     .then((response) => {
-      let result = Math.round((100 * eventThisWeek()) / response.goals);
+      let result = Math.round((100 * passedEvents()) / response.goals);
       if (result <= 100) {
         res.json(result);
       }
