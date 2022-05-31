b6d3f30b316d9ce38c8a2b29aac165d514410622
Chloé Faurie <chloe.faurie@gmail.com>
2022-05-31 10:37:18 +0100

    routePutEventEventId done

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,7 +1,62 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const mongoose = require("mongoose");
+//importing the Event model
+const Event = require("./models/Event");
 
-router.get('/events', (req, res, next) => {
-  res.status(200).json('Should send all the events');
+router.put("/events/:id", (req, res, next) => {
+  const { id } = req.params;
+  const {
+    title,
+    description,
+    location,
+    category,
+    price,
+    startDate,
+    enDate,
+    endDate,
+    image,
+    attendees,
+  } = req.body;
+  //handle optional fields:
+  if (endDate) {
+    endDate = endDate;
+  } else {
+    endDate = null;
+  }
+  if (image) {
+    image = image;
+  } else {
+    image = "";
+  }
+
+  Event.findByIdAndUpdate(
+    id,
+    {
+      title,
+      description,
+      location,
+      category,
+      price,
+      startDate,
+      endDate,
+      image,
+      attendees,
+    },
+    { new: true }
+  )
+    .then((modifiedEvent) => {
+      res.status(200).json(modifiedEvent);
+      return modifiedEvent;
+    })
+    .catch((err) => {
+      if (id === undefined) {
+        res.status(400).json({ message: "Invalid ID supplied" });
+      } else if (!modifiedEvent) {
+        res.status(404).json({ message: "Event not found" });
+      } else {
+        res.status(405).json({ message: "Validation exception" });
+      }
+    });
 });
 
 module.exports = router;
