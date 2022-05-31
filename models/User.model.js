c7d6083af6f72c9d816e01548d2782fbf8aa80d7
Marina Palma <marina_palma@hotmail.com>
2022-05-31 10:19:33 +0100

    fixed bugs

--- models/User.model.js
+++ models/User.model.js
@@ -2,46 +2,50 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-    username: {
-      type: String,
-      unique: true,
-      required: true,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: {
-      type: String,
-      required:true,
-    } ,
-    email: {
-      type: String,
-      required:true
-    },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    // unique: true -> Ideally, should be unique, but its up to you
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
 
   image: {
     type: String,
   },
-  favourite: {
+  favourites: {
     type: [String],
   },
-  goal: {integer},
-  attending: [{
-    type: Schema.Types.ObjectId,
-      ref: "Events",
-  }],
-  createdEvents:[{
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  }],
-  friend: [{
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  }],
-    
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
+  goals: { type: Number },
+  attending: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Event",
+    },
+  ],
+  createdEvents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
 
-);
+  // this second object adds extra properties: `createdAt` and `updatedAt`
+  timestamps: true,
+});
 
 const User = model("User", userSchema);
 
