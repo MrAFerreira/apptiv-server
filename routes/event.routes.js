1f6d720f0afbb445b22d2d966e5c6d2ca5cb2c7c
Chloé Faurie <chloe.faurie@gmail.com>
2022-05-31 09:55:44 +0100

    routePostEvent in progress

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,7 +1,38 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const mongoose = require("mongoose");
+//importing the Event model
+const Event = require("./models/Event");
 
-router.get('/events', (req, res, next) => {
-  res.status(200).json('Should send all the events');
+router.get("/events", (req, res, next) => {
+  Event.find({})
+    .then((allEvents) => {
+      //console.log(allEvents);
+      res.status(200).json(allEvents);
+    })
+    .catch((err) => res.status(400).json({ message: "No events were found" }));
+});
+
+//create the post route:
+router.post("/events", (req, res, next) => {
+  const {
+    id,
+    title,
+    description,
+    location,
+    category,
+    price,
+    startDate,
+    enDate,
+    image,
+    attendees,
+  } = req.body;
+
+  Event.create()
+    .then((eventCreated) => {
+      //console.log(allEvents);
+      res.status(200).json(eventCreated);
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
