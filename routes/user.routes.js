c0e4cf3e9caaa6c3c0178bc8f9262dcb0fea9c86
FranciscoGBento <f_m_bento@hotmail.com>
2022-05-31 10:03:13 +0100

    userRouteFriends

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
-router.get('/user/:userId', (req, res, next) => {
-  res.status(200).json('Should get the specific user info');
+const User = require('../models/User.model')
+
+router.get('/user/:userId/friends', (req, res, next) => {
+  User.findById((userId))
+  .populate("friends")
+  .then((foundUser) => {
+    res.status(200).json(foundUser)
+  })
+  .catch((err) => res.status(400).json({message: "Friends not found!"}))
+  
 });
 
 module.exports = router;
