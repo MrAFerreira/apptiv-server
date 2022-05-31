ba7f3463371776da9dabbead09a2afe3042ee76f
keven andrade <keven111299@gmail.com>
2022-05-31 09:39:59 +0100

    cloudinary config done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const fileUploader = require("../config/cloudinary.config");
 
-router.get('/user/:userId', (req, res, next) => {
-  res.status(200).json('Should get the specific user info');
+router.get("/user/:userId", (req, res, next) => {
+  res.status(200).json("Should get the specific user info");
 });
 
 module.exports = router;
